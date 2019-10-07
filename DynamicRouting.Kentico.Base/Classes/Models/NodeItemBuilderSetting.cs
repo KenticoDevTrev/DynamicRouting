b80bace70f61a9a8a1b01694a4bef6bc3c2e8649
Trevor Fayas - Kentico MVP <tfayas@hbs.net>
2019-10-06 23:46:02 -0500

    Adjusted nuget packages now that online

--- DynamicRouting.Kentico.Base/Classes/Models/NodeItemBuilderSetting.cs
+++ DynamicRouting.Kentico.Base/Classes/Models/NodeItemBuilderSetting.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using CMS.MacroEngine;
+using CMS.SiteProvider;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace DynamicRouting
 {
