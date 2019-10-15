f5fa29e3f8b06c87b4a1aa284ee2251bed7917f6
Trevor Fayas <tfayas@hbs.net>
2019-10-15 14:52:39 -0500

    Add Static Route Priority Attribute

---
+++ DynamicRouting.Kentico.MVC/StaticRoutePriorityAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DynamicRouting.Kentico.MVC
+{
+    /// <summary>
+    /// This attribute indicates that Routes that map to this Controller should be enforced even if a page is found via Dynamic Routing.
+    /// Example: If the Controller is ApiController and a request comes through as /api/GetItems, without this attribute if someone creates
+    /// a page that has a UrlSlug of /api/GetItems then the request would be dynamically routed.  With this attribute, the request would
+    /// properly go to your ApiController class.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class)]
+    public class StaticRoutePriorityAttribute : Attribute
+    {
+    }
+}
---
+++ DynamicRouting.Kentico.MVC/StaticRoutePriorityConstraint.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using CacheHelper = CMS.Helpers.CacheHelper;
+using CacheSettings = CMS.Helpers.CacheSettings;
+
+namespace DynamicRouting.Kentico.MVC
+{
+    /// <summary>
+    /// Checks if the Controller the route is mapping to has the StaticRoutePriorityAttribute
+    /// </summary>
+    public class StaticRoutePriorityConstraint : IRouteConstraint
+    {
+        public bool Match(HttpContextBase httpContext, Route route, string parameterName, RouteValueDictionary values, RouteDirection routeDirection)
+        {
+            // Check if the controller is found and has the KMVCRouteOverPathPriority attribute.
+            string ControllerName = (values.ContainsKey("controller") ? values["controller"].ToString() : "");
+            return CacheHelper.Cache(cs =>
+            {
+                // Check if the Route that it found has the override
+                IControllerFactory factory = ControllerBuilder.Current.GetControllerFactory();
+                try
+                {
+                    var Controller = factory.CreateController(new RequestContext(httpContext, new RouteData(route, null)), ControllerName);
+                    return Attribute.GetCustomAttribute(Controller.GetType(), typeof(StaticRoutePriorityAttribute)) != null;
+                }
+                catch (Exception)
+                {
+                    return false;
+                }
+            }, new CacheSettings(1440, "StaticRoutePriorityConstraint", ControllerName));
+        }
+    }
+
+    
+}
