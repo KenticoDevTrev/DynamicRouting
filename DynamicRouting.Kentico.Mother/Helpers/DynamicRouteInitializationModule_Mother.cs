bbec65e655e1e9b7eac8d3753729ac3474c0f356
Trevor Fayas <tfayas@hbs.net>
2019-10-29 12:56:24 -0500

    Moved Test page to Solution, added start site zip

--- DynamicRouting.Kentico.Mother/Helpers/DynamicRouteInitializationModule_Mother.cs
+++ DynamicRouting.Kentico.Mother/Helpers/DynamicRouteInitializationModule_Mother.cs
@@ -1,5 +1,6 @@
 ﻿using CMS;
 using CMS.DataEngine;
+using CMS.Modules;
 using DynamicRouting.Kentico.Mother;
 
 [assembly: RegisterModule(typeof(DynamicRouteInitializationModule_Mother))]
@@ -20,6 +21,22 @@ namespace DynamicRouting.Kentico.Mother
             // Call OnInit of the Base
             var BaseInitializationModule = new DynamicRouteInitializationModule_Base();
             BaseInitializationModule.Init();
+
+            // Nuget Manifest Build
+            ModulePackagingEvents.Instance.BuildNuSpecManifest.After += BuildNuSpecManifest_After;
+        }
+
+        private void BuildNuSpecManifest_After(object sender, BuildNuSpecManifestEventArgs e)
+        {
+            if (e.ResourceName.Equals("DynamicRouting.Kentico", System.StringComparison.InvariantCultureIgnoreCase))
+            {
+                e.Manifest.Metadata.Owners = "Kentico Community";
+                e.Manifest.Metadata.ProjectUrl = "https://github.com/KenticoDevTrev/DynamicRouting";
+                e.Manifest.Metadata.IconUrl = "http://www.kentico.com/favicon.ico";
+                e.Manifest.Metadata.Copyright = "Copyright 2019 Kentico Community";
+                e.Manifest.Metadata.Title = "Dynamic Routing for Kentico v12 SP";
+                e.Manifest.Metadata.ReleaseNotes = "BETA Version";
+            }
         }
     }
 }
---
+++ DynamicRouting.Kentico.Mother/Testing/Testing.cs
@@ -0,0 +1,55 @@
+﻿using CMS.Base;
+using CMS.DocumentEngine;
+using CMS.EventLog;
+using CMS.SiteProvider;
+using DynamicRouting;
+using DynamicRouting.Kentico;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CMSApp.CMSModules.DynamicRouting
+{
+    public partial class Testing : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnRebuildSite_Click(object sender, EventArgs e)
+        {
+            //EventLogProvider.LogInformation("DynamicRouteTesting", "TestStart", eventDescription: DateTime.Now.ToString() + " " + DateTime.Now.Millisecond.ToString());
+            DynamicRouteInternalHelper.RebuildRoutesBySite(SiteContext.CurrentSiteName);
+            //EventLogProvider.LogInformation("DynamicRouteTesting", "TestEnd", eventDescription: DateTime.Now.ToString() + " " + DateTime.Now.Millisecond.ToString());
+        }
+
+        protected void btnRebuildSubTree_Click(object sender, EventArgs e)
+        {
+            TreeNode Page = DocumentHelper.GetDocuments().Path(tbxPath.Text).FirstOrDefault();
+
+            if (Page != null)
+            {
+                DynamicRouteInternalHelper.RebuildSubtreeRoutesByNode(Page.NodeID);
+            }
+        }
+
+        protected void btnRunQueue_Click(object sender, EventArgs e)
+        {
+            DynamicRouteInternalHelper.CheckUrlSlugGenerationQueue();
+        }
+
+        protected void btnCheckUrl_Click(object sender, EventArgs e)
+        {
+            ITreeNode Node = DynamicRouteHelper.GetPage(tbxRouteToTest.Text);
+            if (Node != null)
+            {
+                ltrPageFound.Text = $"FOUND! {Node.NodeAliasPath} {Node.ClassName} {Node.DocumentCulture}";
+            }
+            else
+            {
+                ltrPageFound.Text = "No Node Found";
+            }
+        }
+    }
+}
