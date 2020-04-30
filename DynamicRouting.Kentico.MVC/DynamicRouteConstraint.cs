d05e2944cec844e81e33bf1b5964b8b5960c991d
Trevor Fayas <tfayas@hbs.net>
2020-04-30 15:32:39 -0500

    Updated DynamicRouteConstraint so it would not be triggered on UrlGeneration requests, which was causing the Url.Action to not work properly.

--- DynamicRouting.Kentico.MVC/DynamicRouteConstraint.cs
+++ DynamicRouting.Kentico.MVC/DynamicRouteConstraint.cs
@@ -27,6 +27,12 @@ namespace DynamicRouting.Kentico.MVC
         /// <returns></returns>
         public bool Match(HttpContextBase httpContext, Route route, string parameterName, RouteValueDictionary values, RouteDirection routeDirection)
         {
+            // Never match on URL generation, this is called with the Url.Action and HtmlLink and shouldn't ever be used.
+            if(routeDirection == RouteDirection.UrlGeneration)
+            {
+                return false;
+            }
+
             string controllerName = values.ContainsKey("controller")
                     ? ValidationHelper.GetString(values["controller"], "")
                     : "";
